57e476a83ae84bef836fcc2d4eb5fef178ed423a
pgartrell <preciousrg@outlook.com>
2022-01-08 16:42:56 -0500

    Half Way

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const productsRoute = require("./routes/product")
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ mongoose
 app.use(express.json()) //This is so it can take JSON
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/products", productsRoute)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend Server is running!")
--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,54 @@
-const router = require("express").Router()
-const User = require("../models/User")
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-//***Note: Extra task is to create an if statement saying if one of the input methods are not entered send status code. 
+
 //REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password,
-    })
-    
-    //Since save is so quick and will execute before the newUser const, we have to do it as a promise
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(req.body.password), process.env.PASS_SEC).toString()
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
     try{
-        const savedUser = await newUser.save()
-        res.status(201).json(savedUser)
-    }catch(err){
+        const user = await User.findOne({username:req.body.username})
+        !user && res.status(401).json("Wrong Credentials")
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+        originalPassword !==req.body.password && res.status(401).json('Wrong Credentials!')
+
+          const accessToken = jwt.sign({
+            id: user._id, 
+            isAdmin: user.isAdmin
+          }, 
+          
+          process.env.JWT_SEC,
+          {expiresIn:"3d"}
+          )
+
+        
+
+        const { password, ...others } = user._doc
+
+        res.status(200).json({...others, accessToken})
+    } catch(err){
         res.status(500).json(err)
     }
 })
-
 module.exports = router
